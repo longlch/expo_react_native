b4884920e5b032c42c153ba946824a4ae02f494f
hoanglonglch <hoanglonglch@gmail.com>
2019-11-03 10:59:26 +0700

    Add todo item

--- app/components/TodoItem.js
+++ app/components/TodoItem.js
@@ -1,12 +1,62 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View} from "react-native";
+import {StyleSheet, Text, View, TouchableOpacity, Dimensions } from "react-native";
+import {circleActive, circleInactive, deleteIconColor, lightWhite} from "../utils/Colors";
+import { MaterialIcons, FontAwesome,Ionicons } from '@expo/vector-icons';
+
+const { height, width } = Dimensions.get('window');
 
 export default class TodoItem extends Component {
 
+    state = {
+        isChecked: false
+    }
+
+    checkCircle = () =>{
+        console.log('log');
+        this.setState({
+            isChecked: !this.state.isChecked
+        });
+    }
+
+
     render() {
+        const {isChecked} = this.state;
         return (
             <View style={styles.container}>
-                <Text>List Here</Text>
+
+                <View style={styles.containerTodo}>
+                    <TouchableOpacity onPress={this.checkCircle}>
+                        <View style={[
+                            styles.circle,
+                            this.state.isChecked ? {borderColor: circleActive} : {
+                                borderColor: circleInactive
+                            }
+                        ]}/>
+                    </TouchableOpacity>
+
+                    <View style={styles.viewTodo}>
+                        <Text style={styles.text}>lorem</Text>
+                    </View>
+
+                </View>
+
+
+                <View style={styles.containerTodo}>
+
+                    {
+                        isChecked &&
+                        <View>
+                            <TouchableOpacity>
+                                <Ionicons name="md-trash" size={25} color={deleteIconColor} />
+                            </TouchableOpacity>
+                        </View>
+                    }
+
+                </View>
+
+
+
+
             </View>
         );
     }
@@ -15,13 +65,29 @@ export default class TodoItem extends Component {
 const styles = StyleSheet.create({
     container:{
         marginTop: 40,
-        alignItems: 'center'
+        backgroundColor: lightWhite,
+        padding: 5,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+
+
     },
     text: {
-        fontSize: 19,
-        fontWeight: 'bold',
-        color: 'white'
-
+        fontWeight: '100',
+        fontSize: 16,
+    },
+    containerTodo: {
+        flexDirection: 'row',
+    },
+    viewTodo:{
+      marginTop: 4
+    },
+    circle: {
+        borderWidth: 3,
+        marginRight: 10,
+        width: 30,
+        height: 30,
+        borderRadius: 15
     }
 });
 
